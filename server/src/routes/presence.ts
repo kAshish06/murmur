fe09f7918bcb225951fa8858e26d1c55d1b153cd
kAshish06 <ashish.kumar.0611@gmail.com>
2025-06-26 07:56:56 +0530

    added api to get presence status for many users.

--- server/src/routes/presence.ts
+++ server/src/routes/presence.ts
@@ -3,6 +3,7 @@ import { param, validationResult } from "express-validator";
 import { getUserPresence, getUsersPresence } from "../services/presenceService";
 import { AuthenticatedRequest, verifyToken } from "../middleware/verifyToken";
 import HTTP_STATUS_CODE_MAP from "../utils/httpStatusCodeMap";
+import { query } from "winston";
 
 const router = Router();
 
@@ -28,6 +29,36 @@ router.get(
         return;
       }
       res.status(HTTP_STATUS_CODE_MAP.SUCCESS).json(userPresence);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get(
+  "/group",
+  verifyToken,
+  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    try {
+      const userIds = req.query.userIds;
+      if (!userIds || typeof userIds !== "string") {
+        res
+          .status(HTTP_STATUS_CODE_MAP.BAD_REQUEST)
+          .json({ error: "Incorrect user ids query param" });
+        return;
+      }
+      const convertedUserIds = userIds
+        .split(",")
+        .map((id) => Number(id))
+        .filter((id) => Number.isInteger(id));
+      const usersPresence = await getUsersPresence(convertedUserIds);
+      if (!usersPresence) {
+        res
+          .status(HTTP_STATUS_CODE_MAP.INTERNAL_ERROR)
+          .json({ error: "Error encountered in fetching users presence data" });
+        return;
+      }
+      res.status(HTTP_STATUS_CODE_MAP.SUCCESS).json(usersPresence);
     } catch (error) {
       next(error);
     }

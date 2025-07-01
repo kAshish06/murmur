4116d2b2273dd173967aad324605001b1d0f113a
kAshish06 <ashish.kumar.0611@gmail.com>
2025-07-01 22:21:32 +0530

    Fixed dateFormatter.

--- client/src/utils/dateFormatter.ts
+++ client/src/utils/dateFormatter.ts
@@ -3,10 +3,14 @@ import { DAY } from "../constants";
 /** Returns Today, Yesterday or date in dd/mm/yyyy format */
 export const getDateString = (dateString: string) => {
   const date = new Date(dateString);
-  const today = new Date();
-  if (date.getDate() - today.getDate() === 0) {
-    return DAY.TODAY;
-  } else if (date.getDate() - today.getDate() === 1) {
+  const rawDateTime = date.getTime();
+  const nowDateTime = new Date().getTime();
+  const diffInDays = Math.floor(
+    (nowDateTime - rawDateTime) / (1000 * 60 * 60 * 24)
+  );
+  if (diffInDays < 1) {
+    return `${date.getHours()}:${date.getMinutes()}`;
+  } else if (diffInDays > 1 && diffInDays < 2) {
     return DAY.YESTERDAY;
   }
   return date.toLocaleDateString();

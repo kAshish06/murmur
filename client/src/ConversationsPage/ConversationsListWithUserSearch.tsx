eb651909d67246d8a2e2893e2b79e8b9f41f1863
kAshish06 <ashish.kumar.0611@gmail.com>
2025-07-19 07:08:51 +0530

    Renamed old ConversationList component and replaced Conversation List related code with the ConversationList component.

--- client/src/ConversationsPage/ConversationsListWithUserSearch.tsx
+++ client/src/ConversationsPage/ConversationsListWithUserSearch.tsx
@@ -1,46 +1,29 @@
-import { useEffect, useState, useRef } from "react";
-import { useGetConversations } from "./query/conversationsQuery";
+import { useState, useRef } from "react";
 import SearchUser, {
   type SearchUserRef,
 } from "../components/customUtils/SearchUser";
-import useConversationsStore from "../store/useConversationsStore";
-import { getDateString } from "../utils/dateFormatter";
 import { type Conversation } from "./types";
 import { type User } from "../Auth/types";
 import { useAuthStore } from "../store/useAuthStore";
 import { SearchedUserList } from "../components/customUtils/SearchedUserList";
+import ConversationList from "./ConversationList";
+import useConversationsStore from "../store/useConversationsStore";
 
 type Props = {
   onConversationSelection: (conversation: Conversation) => void;
 };
 
-export default function ConversationsList({ onConversationSelection }: Props) {
+export default function ConversationsListWithUserSearch({
+  onConversationSelection,
+}: Props) {
   const [searchQuery, setSearchQuery] = useState("");
   const searchUserRef = useRef<SearchUserRef>(null);
   const {
     conversations: storedConversations,
-    selectedConversationId,
-    setConversations,
     addConversation,
     setSelectedConversationId,
   } = useConversationsStore();
   const { user: currentUser } = useAuthStore();
-  const {
-    data: fetchedConversations,
-    isPending,
-    isError,
-  } = useGetConversations();
-
-  useEffect(() => {
-    if (!isPending && fetchedConversations?.length) {
-      setConversations(fetchedConversations);
-    }
-  }, [fetchedConversations, isPending, setConversations]);
-
-  const handleConversationClick = (conversation: Conversation) => {
-    setSelectedConversationId(conversation.clientId);
-    onConversationSelection(conversation);
-  };
 
   const handleSearchedUserClick = async (user: User) => {
     if (!currentUser?.id) {
@@ -71,11 +54,6 @@ export default function ConversationsList({ onConversationSelection }: Props) {
     searchUserRef.current?.closeSearch();
   };
 
-  if (isPending) return <div>Loading conversations...</div>;
-  if (isError)
-    return <div>Error loading conversations. Please try again later.</div>;
-  if (!storedConversations?.length) return <div>No conversations found.</div>;
-
   return (
     <div className="space-y-2 px-1">
       <h2 className="text-xl font-semibold text-left p-4 pl-2">
@@ -91,29 +69,9 @@ export default function ConversationsList({ onConversationSelection }: Props) {
         searchQuery={searchQuery}
         handleUserClick={handleSearchedUserClick}
       />
-      {!searchQuery &&
-        storedConversations.map((conversation) => (
-          <div
-            key={conversation.clientId}
-            className={`${
-              selectedConversationId === conversation.clientId
-                ? "bg-gray-900 text-white hover:bg-black"
-                : "hover:bg-gray-200"
-            } px-3 py-3 rounded-lg cursor-pointer transition-colors duration-200`}
-            onClick={() => handleConversationClick(conversation)}
-          >
-            <div className="flex items-center justify-between">
-              <div>
-                <h3 className="font-semibold">
-                  {conversation.otherParticipants[0].username}
-                </h3>
-              </div>
-              <span className="text-xs">
-                {getDateString(conversation.updatedAt)}
-              </span>
-            </div>
-          </div>
-        ))}
+      {!searchQuery && (
+        <ConversationList onConversationSelection={onConversationSelection} />
+      )}
     </div>
   );
 }
